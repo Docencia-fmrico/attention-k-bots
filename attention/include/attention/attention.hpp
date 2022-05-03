4a3fefa8650cb5d2da5b0d9220d4d48360958a08
Angelo Vincenzo Barra <av.barra.2019@alumnos.urjc.es>
2022-05-03 18:01:20 +0200

    Something that compiles

--- attention/include/attention/attention.hpp
+++ attention/include/attention/attention.hpp
@@ -2,24 +2,31 @@
 
 #define ATTENTION__ATTENTION_HPP_
 
-
-
-#include "ros2_knowledge_graph/GraphNode.hpp"
 #include "gazebo_msgs/msg/model_states.hpp"
-
+#include "lifecycle_msgs/msg/state.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "ros2_knowledge_graph/GraphNode.hpp"
 using std::placeholders::_1;
 
-class AttentionNode : public rclcpp::Node
-{
-public:
-  AttentionNode();
+class AttentionNode : public rclcpp_lifecycle::LifecycleNode {
+   public:
+    AttentionNode();
 
-private:
-  void GazeboCallback(const gazebo_msgs::msg::ModelStates::SharedPtr msg); 
-  std::shared_ptr<ros2_knowledge_graph::GraphNode> graph_;
-  rclcpp::Subscription<gazebo_msgs::msg::ModelStates>::SharedPtr gazeboSub_;
+    using CallbackReturnT = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
+    CallbackReturnT on_configure(const rclcpp_lifecycle::State& state);
+    CallbackReturnT on_activate(const rclcpp_lifecycle::State& state);
+    CallbackReturnT on_deactivate(const rclcpp_lifecycle::State& state);
+    CallbackReturnT on_cleanup(const rclcpp_lifecycle::State& state);
+    CallbackReturnT on_shutdown(const rclcpp_lifecycle::State& state);
+    CallbackReturnT on_error(const rclcpp_lifecycle::State& state);
+
+   private:
+    void GazeboCallback(const gazebo_msgs::msg::ModelStates::SharedPtr msg);
+    std::shared_ptr<ros2_knowledge_graph::GraphNode> graph_;
+    bool graph_initialized_ = false;
+    rclcpp::Subscription<gazebo_msgs::msg::ModelStates>::SharedPtr gazeboSub_;
 };
-
 
 #endif  // ATTENTION__ATTENTION_HPP_
--- attention/src/attention/attention.cpp
+++ attention/src/attention/attention.cpp
@@ -1,21 +1,98 @@
+#include "attention/attention.hpp"
+
+#include "gazebo_msgs/msg/model_states.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "lifecycle_msgs/msg/state.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_lifecycle/lifecycle_node.hpp"
-#include "attention/attention.hpp"
-
-#include "gazebo_msgs/msg/model_states.hpp"
 #include "ros2_knowledge_graph/GraphNode.hpp"
+#include "ros2_knowledge_graph/graph_utils.hpp"
+#include "ros2_knowledge_graph_msgs/msg/edge.hpp"
+#include "ros2_knowledge_graph_msgs/msg/graph.hpp"
+#include "ros2_knowledge_graph_msgs/msg/node.hpp"
+
 using std::placeholders::_1;
 
-AttentionNode::AttentionNode() : rclcpp_lifecycle::LifecycleNode("node_Attention") {
-  
-  graph_ = std::make_shared<ros2_knowledge_graph::GraphNode>(shared_from_this());
+using CallbackReturnT = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
 
-  gazeboSub_ = this->create_subscription<gazebo_msgs::msg::ModelStates>(
-      "/gazebo/model_states", 10, std::bind(&AttentionNode::GazeboCallback, this, _1));
+AttentionNode::AttentionNode() : rclcpp_lifecycle::LifecycleNode("graph_filliing_node") {
+    gazeboSub_ = this->create_subscription<gazebo_msgs::msg::ModelStates>(
+        "/gazebo/model_states", 10, std::bind(&AttentionNode::GazeboCallback, this, _1));
 }
 
 void AttentionNode::GazeboCallback(const gazebo_msgs::msg::ModelStates::SharedPtr msg) {
-  std::cout << msg->name.at(0) << std::endl;
+    if (!graph_initialized_) return;
+
+    for (int i = 0; i < msg->name.size(); i++){
+      ros2_knowledge_graph_msgs::msg::Node object = ros2_knowledge_graph::new_node(msg->name.at(i), "object");
+      graph_->update_node(object);
+
+      geometry_msgs::msg::TransformStamped tf;
+      tf.header.frame_id = "map";
+      tf.header.stamp = this->get_clock()->now();
+      tf.child_frame_id = msg->name.at(i);
+      tf.transform.translation.x = msg->pose.at(i).position.x;
+      tf.transform.translation.y = msg->pose.at(i).position.y;
+      tf.transform.translation.z = msg->pose.at(i).position.z;
+      
+      auto edge_tf = ros2_knowledge_graph::new_edge("map", msg->name.at(i), tf);
+      graph_->update_edge(edge_tf);
+    }
+}
+
+CallbackReturnT AttentionNode::on_configure(const rclcpp_lifecycle::State& state) {
+    RCLCPP_INFO(
+        get_logger(), "[%s] Configuring from [%s] state...", get_name(),
+        state.label().c_str());
+
+    graph_ = std::make_shared<ros2_knowledge_graph::GraphNode>(shared_from_this());
+    
+
+    ros2_knowledge_graph_msgs::msg::Node robot = ros2_knowledge_graph::new_node("kbot", "robot");
+    graph_->update_node(robot);
+
+    ros2_knowledge_graph_msgs::msg::Node map = ros2_knowledge_graph::new_node("map", "map");
+    graph_->update_node(map);
+    //graph_initialized_ = true;
+    return CallbackReturnT::SUCCESS;
+}
+CallbackReturnT AttentionNode::on_activate(const rclcpp_lifecycle::State& state) {
+    RCLCPP_INFO(
+        get_logger(), "[%s] Activating from [%s] state...", get_name(),
+        state.label().c_str());
+
+    return CallbackReturnT::SUCCESS;
+}
+
+CallbackReturnT AttentionNode::on_deactivate(const rclcpp_lifecycle::State& state) {
+    RCLCPP_INFO(
+        get_logger(), "[%s] Deactivating from [%s] state...", get_name(),
+        state.label().c_str());
+
+    return CallbackReturnT::SUCCESS;
+}
+
+CallbackReturnT AttentionNode::on_cleanup(const rclcpp_lifecycle::State& state) {
+    RCLCPP_INFO(
+        get_logger(), "[%s] Cleanning Up from [%s] state...", get_name(),
+        state.label().c_str());
+
+    return CallbackReturnT::SUCCESS;
+}
+
+CallbackReturnT AttentionNode::on_shutdown(const rclcpp_lifecycle::State& state) {
+    RCLCPP_INFO(
+        get_logger(), "[%s] Shutting Down from [%s] state...", get_name(),
+        state.label().c_str());
+
+    return CallbackReturnT::SUCCESS;
+}
+
+CallbackReturnT AttentionNode::on_error(const rclcpp_lifecycle::State& state) {
+    RCLCPP_INFO(
+        get_logger(), "[%s] Shutting Down from [%s] state...", get_name(),
+        state.label().c_str());
+    return CallbackReturnT::SUCCESS;
 }
